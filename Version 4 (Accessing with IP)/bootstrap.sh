b26212f967d3aa81dd56376eb76574f081d85815
abhijeet <abhijeet.kamble619@gmail.com>
2014-08-29 21:45:51 +0530

    adding initial Vagrant files with 4 versions

---
+++ Version 4 (Accessing with IP)/bootstrap.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+sudo apt-get update 
+
+echo "Enabling Provisioning with Vagrant"
+apt-get install -y apache2
+echo "...........1%"
+echo "...........40%"
+echo "...........60%"
+echo "...........70%"
+echo "...........80%"
+echo "...........90%"
+echo "...........100%"
+echo "Apache Installed"
+echo "Provisioning Completed"
+
